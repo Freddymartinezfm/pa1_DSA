069171a8ad89bda5d1c781c31c837a9547dc3e39
freddymartinezfm1 <38080761+freddymartinezfm1@users.noreply.github.com>
2020-01-18 22:45:58 -0600

    cleanup finished ready to move to cs server

--- main.cpp
+++ main.cpp
@@ -22,76 +22,62 @@ int main(){
 
 
 std::string input(){
-		std::string m;
-		while ((std::cin >> m).good()){
-			if (m.length() != 1){
-				std::cin.clear();
-				std::cin.ignore(256, '\n');
-				std::cout << "Invalid selection: too many characters " << std::endl;
+	std::string m;
+	while ((std::cin >> m).good()){
+		if (m.length() != 1){
+			std::cin.clear();
+			std::cin.ignore(256, '\n');
+			std::cout << "Invalid selection: too many characters " << std::endl;
 		} else {
 			return m;
 		}
 	}
-	return NULL;
+	return "Invalid";
 }
 
 void build(std::string& selection){
-	
 	if (selection == "i"){
-			std::cout << "Enter new integer keys to insert. Enter 'q' to quit " << std::endl;
-			std::string key;
-			
-			while(std::cin >> key){	
-				if (key == "q"){
-					break;
-				}
-				try {
-					searchTree.insert(std::stoi(key));
-
-				} catch (exception &e){
-					std::cout << "Invalid input to insert: '" << e.what() << "'" << " error.";
-				}	
-				searchTree.print();
+		std::cout << "Enter new integer keys to insert. Enter 'q' to quit " << std::endl;
+		std::string key;
+		
+		while(std::cin >> key){	
+		if (key == "q") break;
+			try {
+				searchTree.insert(std::stoi(key));
+			} catch (exception &e){
+				std::cout << "Invalid input to insert: '" << e.what() << "'" << " error.";
 			}
-				// menuOptions.menu();
-			
-		} else if (selection == "d"){
-			std::cout << "Enter new integer keys to delete. Enter 'q' to quit " << std::endl;
-			std::string key;
-			
-			while(std::cin >> key){	
-				if (key == "q"){
-					std::cout << "quit";
-					// menuOptions.menu();
-					break;
-				}
-				try {
-					searchTree.remove(std::stoi(key));
-				} catch (exception &e){
-					std::cout << "Invalid input to remove: '" << e.what() << "'" << " error. ";
-				}	
-				searchTree.print();
-			}
-
-		} else if (selection == "h"){
-			std::cout << "The height of the binary tree is " <<  searchTree.height() << std::endl;
-			// menuOptions.menu();
-		}  else if (selection == "s"){
-			std::cout << "The size of the binary tree is " <<  searchTree.size() << std::endl;
-			menuOptions.menu();
-		} else if (selection == "c"){
-			searchTree.clear();
-			// menuOptions.menu();
-		} else if (selection == "p"){
-			searchTree.print();
-			// menuOptions.menu();
-		} else if (selection == "x"){
-			std::cout << "Program ended " << std::endl;
-			running = false;
-			exit(1);
-		} else {
-			std::cout << "Not an option " << std::endl;
-
+		searchTree.print();	
 		}
-
+	} else if (selection == "d"){
+		std::cout << "Enter new integer keys to delete. Enter 'q' to quit " << std::endl;
+		std::string key;
+		
+		while(std::cin >> key){	
+			if (key == "q") break;
+			try {
+				searchTree.remove(std::stoi(key));
+			} catch (exception &e){
+				std::cout << "Invalid input to remove: '" << e.what() << "'" << " error. ";
+			}	
+		}
+	} else if (selection == "h"){
+		searchTree.print();
+		std::cout << "The height of the binary tree is " <<  searchTree.height() << std::endl;
+	}  else if (selection == "s"){
+		searchTree.print();
+		std::cout << "The size of the binary tree is " <<  searchTree.size() << std::endl;
+	} else if (selection == "c"){
+		std::cout << "Clearing... ";
+		searchTree.print();
+		searchTree.clear();
+	} else if (selection == "p"){
+		searchTree.print();
+	} else if (selection == "x"){
+		std::cout << "Program ended " << std::endl;
+		running = false;
+		exit(1);
+	} else {
+		std::cout << "Not an option " << std::endl;
+	}
 }
